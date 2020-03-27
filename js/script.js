361579b9c1ee1668bb88ab8cc4706a623a27851a
Viktoriya Kass <vikavika233@gmail.com>
2020-03-27 22:56:46 +0400

    второе дз

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 
-let money, income, addExpenses, deposit, mission, period;
+let money = 1600;
+let income = 'фриланс';
 
-alert('Привет, дружище');
-console.log('Вот мой любой текст');
+let addExpenses = 'Интернет, автобусы, кафешки';
+let deposit = true;
+let mission = 500;
+let period = 2;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+
+addExpenses = addExpenses.toLowerCase();
+
+for (let i = 0; i < addExpenses.length; i++) {
+    console.log( addExpenses[i]) ;
+}
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
+
