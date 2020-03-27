c502a450f5f9615e7626d4c7e415f94a81f841c9
Viktoriya Kass <vikavika233@gmail.com>
2020-03-27 23:45:56 +0400

    extra задание

--- js/script.js
+++ js/script.js
@@ -1,32 +1,23 @@
+let num = 266219;
+let ar = [];
+let m = 1;
 
-let money = 1600;
-let income = 'фриланс';
-let addExpenses = 'Интернет, автобусы, кафешки';
-let deposit = true;
-let mission = 500;
-let period = 2;
 
-/* Вывожу в консоль тип данных значений переменных money, income, deposit; */
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-/* Вывожу в консоль длину строки addExpenses */
-console.log(addExpenses.length);
-
-/* Вывожу в консоль “Период равен (period) месяцев” и “Цель заработать (mission) долларов” */
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' долларов');
-
-/* Привожу строку addExpenses к нижнему регистру */
-addExpenses = addExpenses.toLowerCase();
-
-/* Разбиваю строку на массив и массив в консоль */
-for (let i = 0; i < addExpenses.length; i++) {
-    console.log( addExpenses[i]) ;
+for (let i = 0; i < 6; i++) {
+    ar[i] = num % 10;
+    num = Math.trunc( num / 10 );        
 }
 
-/* Объявить переменную budgetDay, присвоить ей дневной бюджет и вывести в консоль */
-let budgetDay = money / 30;
-console.log('budgetDay: ', budgetDay);
+for (let i = 0; i < 6; i++) {
+  m*=ar[i];     
+}
+
+console.log('Произведение чисел числа 266219: ', m);
+
+m**=3;
+console.log('Возвела в степень 3: ', m);
+
+
+m = m + ''; 
+console.log('Первые 2 цифры числа: ', m.substring(0 , 2) );
 
