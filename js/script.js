84043b71c8793f9a314153d430e85aea3dc3e8d9
Viktoriya Kass <vikavika233@gmail.com>
2020-04-03 01:48:32 +0500

    fixed  getStatusIncome  problem

--- js/script.js
+++ js/script.js
@@ -41,17 +41,14 @@ let budgetDay = Math.floor( budgetMonth/ 30 );
 console.log('Бюджет на день: ', budgetDay);
 
 if (budgetDay>20) {
-   console.log('У вас высокий уровень дохода'); 
-} 
-else if (budgetDay<20 && budgetDay>10) {
-    console.log('У вас средний уровень дохода'); 
-}
-else if (budgetDay<10) {
-    console.log('К сожалению у вас уровень дохода ниже среднего'); 
-}
-else if (budgetDay<0) {
-    console.log('Что то пошло не так');
-}
-else {
- console.log('Ваш дневной бюджет 0, 10 или 20');
-}
+    console.log('У вас высокий уровень дохода'); 
+ } 
+ else if (budgetDay<=20 && budgetDay>10) {
+     console.log('У вас средний уровень дохода'); 
+ }
+ else if (budgetDay<=10 && budgetDay>=0) {
+     console.log('К сожалению у вас уровень дохода ниже среднего'); 
+ }
+ else if (budgetDay<0) {
+     console.log('Что то пошло не так');
+ }
